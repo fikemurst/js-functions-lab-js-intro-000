ec5d59eda0aeae78f10f8abe27246e40073a5f9d
michael furst <michael@furst.us>
2018-05-19 21:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function happyHolidays(){
   return('Happy holidays!')
 }
 function happyHolidaysTo(name) {
-  return("Happy holidays, ${name}!")
+  console.log("Happy holidays, ${name}!")
   happyHolidaysTo('you')
 }
